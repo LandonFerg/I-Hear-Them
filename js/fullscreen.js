d1da8a7fc8451846eb46919752881bbbbb2fcd00
Landon Ferguson <landonljf@gmail.com>
2021-11-10 13:57:48 -0500

    nerfed fullscreen mode

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,21 +1,26 @@
-const fullscreen = false
+let gf = false;
 
-$("#fsButton").click(function(){
-    openFullscreen();
-    fullscreen = true;
-    console.log("fs!");
-});
-
-document.getElementById('holder').addEventListener('fullscreenchange', (event) => 
+$("#fsButton").click(function()
 {
-    if (document.fullscreenElement) {
+    if (!gf) 
+    {
         openFullscreen();
+        console.log("fs!" + gf);
+        gf = true;
     }
+
     else
     {
         closeFullscreen();
+        console.log("nf" + gf);
+        gf = false;
     }
-});
+}); 
+
+// document.getElementById('holder').addEventListener('fullscreenchange', (event) => 
+// {
+
+// });
 
 /* When the openFullscreen() function is executed, open the video in fullscreen.
 Note that we must include prefixes for different browsers, as they don't support the requestFullscreen method yet */
@@ -24,31 +29,44 @@ function openFullscreen()
     var elem = document.getElementById("holder");
     var gameCan = document.getElementById("gameCanvas"); // click to focus
 
-    if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-    } else if (elem.mozRequestFullScreen) { /* Firefox */
-    elem.mozRequestFullScreen();
-    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-    elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) { /* IE/Edge */
-    elem.msRequestFullscreen();
-    }
+    // if (elem.requestFullscreen) {
+    // elem.requestFullscreen();
+    // } else if (elem.mozRequestFullScreen) { /* Firefox */
+    // elem.mozRequestFullScreen();
+    // } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    // elem.webkitRequestFullscreen();
+    // } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    // elem.msRequestFullscreen();
+    // }
     gameCan.style.maxHeight = 'none';
     gameCan.style.maxWidth = 'none';
     
-    gameCan.style.height = '100%';
-    gameCan.style.width = '100%';
+    gameCan.style.height = '720px';
+    gameCan.style.width = '1280px';
+    
+    elem.style.height = '720px';
+    elem.style.width = '1280px';
+    
+    //console.log("f function");
     // 900, 506 | 1067, 600 | 1280, 720
 }
 
 function closeFullscreen()
 {
     var gameCan = document.getElementById("gameCanvas"); // click to focus
+    var elem = document.getElementById("holder");
     //max-width: 900px; max-height: 506px;
 
-    gameCan.style.maxHeight = '506px';
-    gameCan.style.maxWidth = '900px';
+    // gameCan.style.maxHeight = '506px';
+    // gameCan.style.maxWidth = '900px';
+
+    gameCan.style.height = '506px';
+    gameCan.style.width = '900px';
+
+    elem.style.height = '506px';
+    elem.style.width = '900px';
     
-    gameCan.style.height = '100%';
-    gameCan.style.width = '100%';
+    //console.log("nf function")
+    // gameCan.style.height = '100%';
+    // gameCan.style.width = '100%';
 }
